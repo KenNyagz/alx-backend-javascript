33336fc987d18d557ee6fcbd784bae68577ec140
KenNyagz <kennyagz681@gmail.com>
2024-05-27 23:56:41 +0300

    aliased commit

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
-import getBudgetObject from './7-getBudgetObject.js';
+/*import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
@@ -10,6 +10,24 @@ export default function getFullBudgetObject(income, gdp, capita) {
     },
     getIncomeInEuros(income) {
       return `${income} euros`
+    },
+  };
+
+  return fullBudget;
+}*/
+
+/* eslint-disable import/extensions */
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
     },
   };
 
