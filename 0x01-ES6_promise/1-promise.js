21e8a1fb2a992c6d5a8ebc58ac47bebdeb79ac66
KenNyagz <kennyagz681@gmail.com>
2024-05-29 10:40:01 +0300

    aliased commit

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,25 +1,9 @@
-/*export default getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'success'
-      });
-    } else {
-      reject(new Error('The fake api is not working correctly'));
-    }
-  });
-}*/
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
+      resolve({status: 200, body: 'Success' });
     } else {
-      reject(new Error('The API is not working currently'));
+      reject(new Error('The fake API  is not working currently'));
     }
   });
 }
-
