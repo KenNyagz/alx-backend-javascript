2e890ecd001fdf117e928e3edc3f35cfd9eceae6
KenNyagz <kennyagz681@gmail.com>
2024-05-29 16:59:30 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)]
   ).then((results) => {
-    return results.mao((result) => {
+    return results.map((result) => {
       return {
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : result.reason
