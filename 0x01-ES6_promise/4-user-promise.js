b5b7a4b7138f90badc8cbc789789bc9ca468ef37
KenNyagz <kennyagz681@gmail.com>
2024-05-29 16:18:35 +0300

    promise success resolve

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 export default function signUpUser(firstName, lastName) {
   let user = {}
   return new Promise((success, fail) => {
-    if (success) {
+    if (firstName && lastName) {
       user = {
         firstName: firstName,
         lastName: lastName
