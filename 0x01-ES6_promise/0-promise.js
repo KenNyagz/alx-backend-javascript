844efdbb3ff828cd31b4738f30368376379e1068
KenNyagz <kennyagz681@gmail.com>
2024-05-29 09:49:34 +0300

    aliased commit

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,7 +2,7 @@ export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     // Perform API request or any asynchronous operation
     // Example: Fetch data from an API
-    fetch('https://api.example.com/data')
+    fetch('https://api.gateio.ws/api/v4/spot/tickers')
       .then(response => response.json())
       .then(data => {
         // Resolve the Promise with the API response data
