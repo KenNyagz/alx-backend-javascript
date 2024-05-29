3d42f54ab00c34fedf9bd3a0fe66c7cd8768dd76
KenNyagz <kennyagz681@gmail.com>
2024-05-30 00:30:27 +0300

    await & async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils.js'
+
+export default async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+    return {
+      photo,
+      user
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null
+    };
+  }
+}
