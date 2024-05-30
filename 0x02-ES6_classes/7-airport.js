1e1d93234c746fa2f1039a8001079bd4099f7d82
KenNyagz <kennyagz681@gmail.com>
2024-05-31 00:43:02 +0300

    hmmm

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() { return this._name; }
+  get code() { return this._code; }
+
+  set name(name) {
+    this._name = name;
+  }
+  set code(code) {
+    this._code = code;
+  }
+
+  tostring() {
+    return [`${this._code}`];
+  }
+}
