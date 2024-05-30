1a5cf5e34a6b03a6f02dcb97dd09854b97e419f1
KenNyagz <kennyagz681@gmail.com>
2024-05-30 22:23:20 +0300

    class method

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -5,17 +5,18 @@ export default class Currency {
   }
 
   get code() { return this._code; }
+
   get name() { return this._name; }
 
   set code(code) {
-      this._code = code;
+    this._code = code;
   }
+
   set name(name) {
-      this._name = name;
+    this._name = name;
   }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
-
 }
