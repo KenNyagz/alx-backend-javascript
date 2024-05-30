a3e48bcaaa43f38e250c037051fd0319063fcef3
KenNyagz <kennyagz681@gmail.com>
2024-05-30 22:46:26 +0300

    A static method and a useless import

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,29 @@
-`
+// import Currency from './3-currency.js'
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() { return this._amount; }
+
+  get currency() { return this._currency; }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    const { name, code } = this._currency;
+    return `${this.amount} ${name} (${code}) `;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
