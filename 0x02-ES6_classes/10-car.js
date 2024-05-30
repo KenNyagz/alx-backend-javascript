0fb46685dc4f315e13cd7218db7873fd26cb588f
KenNyagz <kennyagz681@gmail.com>
2024-05-31 01:55:40 +0300

    confused

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,20 +1,22 @@
 const cloneSymbol = Symbol('cloneCar');
 
 export default class Car {
-
   constructor(brand, motor, color) {
-    this._brand =  brand;
+    this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  get brand() {return this._brand;}
-  get motor() {return this._motor;}
-  get color() {return this._color;}
+  get brand() { return this._brand; }
+
+  get motor() { return this._motor; }
+
+  get color() { return this._color; }
 
   [cloneSymbol]() {
     return new Car(this._brand, this._motor, this._color);
   }
+
   cloneCar() {
     return this[cloneSymbol]();
   }
