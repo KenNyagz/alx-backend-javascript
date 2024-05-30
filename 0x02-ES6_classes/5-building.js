dfa09cfc11267ffd7930c56ac1dd7aee8ef02b06
KenNyagz <kennyagz681@gmail.com>
2024-05-30 23:41:06 +0300

    abstract class with condition in constructor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,4 +1,15 @@
 export default class Building {
-  constructor
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
 
+  get sqft() { return this._sqft; }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
