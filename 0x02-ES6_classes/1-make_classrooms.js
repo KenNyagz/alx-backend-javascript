7d48703dfc463aec29b58e8ab8e8794a4a4a5335
KenNyagz <kennyagz681@gmail.com>
2024-05-30 20:52:08 +0300

    aliased commit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-export default class initializeRooms() {
-   const ClassRoom1 = new ClassRoom(19);
-   const ClassRoom2 = new ClassRoom(20);
-   const ClassRoom3 = new ClassRoom(34);
-   return [ClassRoom1, ClassRoom2, ClassRoom3];
+export default function initializeRooms() {
+  const ClassRoom1 = new ClassRoom(19);
+  const ClassRoom2 = new ClassRoom(20);
+  const ClassRoom3 = new ClassRoom(34);
+  return [ClassRoom1, ClassRoom2, ClassRoom3];
 }
