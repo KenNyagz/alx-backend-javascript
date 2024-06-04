28c783b141c4a907d0d992cd36fcd64079098d10
KenNyagz <kennyagz681@gmail.com>
2024-06-05 01:04:45 +0300

    slicing an array

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  const validValues = [];
+
+  // Iterate through the set and add the valid values to the array
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      validValues.push(value.slice(startString.length));
+    }
+  }
+
+  // Join the valid values into a string and return it
+  return validValues.join('-');
+}
