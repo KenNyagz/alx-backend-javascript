05673772e2d78bda773af4026d22cdbc848f7789
KenNyagz <kennyagz681@gmail.com>
2024-06-04 23:01:20 +0300

    filter then map then selcet(find)

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,11 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const arr = [];
-  students
-    .filter((student) => student.city === city)
+  return students
+    .filter((student) => student.location === city)
     .map((student) => {
       const newGrade = newGrades.find((grade) => grade.studentId === student.id);
-      arr.push({
-        id: student.id,
-        name: student.name,
-        city: student.city,
-        // ...student,
+      return {
+        ...student,
         grade: newGrade ? newGrade.grade : 'N/A',
-      });
-      return arr;
+      };
     });
-  return arr;
 }
