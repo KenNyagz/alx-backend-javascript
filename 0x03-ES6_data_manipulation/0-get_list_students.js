0375de8e56e398a578998f38d291b3e75286805c
KenNyagz <kennyagz681@gmail.com>
2024-06-04 12:15:07 +0300

    list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const arr = [];
+  arr.push({ firstname: 'Guillame', id: 1, location: 'San Francisco' });
+  arr.push({ firstname: 'James', id: 2, location: 'Columbia' });
+  arr.push({ firstname: 'Serena', id: 5, location: 'San Francisco' });
+  return arr;
+}
