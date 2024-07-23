512691b894e2e81bff5f3283eff3765d5e237a83
KenNyagz <kennyagz681@gmail.com>
2024-07-24 00:47:09 +0300

    Small http server with nodejs

---
+++ 0x05-Node_JS_basic/4-https.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.end('Hello holberton School!');
+});
+
+const port = 1245;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
