bdef8f1fd599aa8ff86b7bb655a4481f18b2baa5
KenNyagz <kennyagz681@gmail.com>
2024-07-24 13:50:01 +0300

    HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,14 +1,13 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-
-const app = http.createServer(async ( req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!'); 
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    studts = await countStudents(process.argv[2]);
-    res.end(studts); 
+    const studts = await countStudents(process.argv[2]);
+    res.end(studts);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
