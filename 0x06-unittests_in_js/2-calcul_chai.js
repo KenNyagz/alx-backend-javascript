cc81390d47dd639a69113302013b12654c27135f
KenNyagz <kennyagz681@gmail.com>
2024-07-25 21:59:30 +0300

    Chai assertion

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,19 @@
+function calculateNumber (type, a, b) {
+  const A = Math.round(a);
+  const B = Math.round(b);
+  let c = 0;
+
+  if (type === 'SUM') {
+    c = A + B;
+  } else if (type === 'SUBTRACT') {
+    c = A - B;
+  } else if (type === 'DIVIDE') {
+    if (B === 0) { return 'Error'; }
+    c = A / B;
+  } else {
+    throw new Error('Invalid operation');
+  }
+  return c;
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,44 @@
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function () {
+  it('tests addition of two positive numbers', function () {
+    expect(calculateNumber('SUM', 3, 5)).to.equal(8);
+    expect(calculateNumber('SUM', 4.7, 10.1)).to.equal(15);
+  });
+
+  it('tests addition of two negatvie numbers', function () {
+    expect(calculateNumber('SUM', -2, -1)).to.equal(-3);
+    expect(calculateNumber('SUM', -3.4, -5.5)).to.equal(-8);
+  });
+
+  it('tests subtraction of two positive numbers', function () {
+    expect(calculateNumber('SUBTRACT', -3, -5)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', -4.5, -5.5)).to.equal(1);
+  });
+
+  it('tests subtraction of two negative numbers', function () {
+    expect(calculateNumber('SUBTRACT', -2, -3)).to.equal(1);
+    expect(calculateNumber('SUBTRACT', -3.1, -3.5)).to.equal(0);
+  });
+  it('tests division of two positive numbers', function () {
+    expect(calculateNumber('DIVIDE', 6, 3)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 6, 3.4)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 6.3, 3.4)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 6.3, 3)).to.equal(2);
+  });
+
+  it('tests division by zero', function() {
+    expect(calculateNumber('DIVIDE', 5, 0)).to.equal('Error'); 
+    expect(calculateNumber('DIVIDE', -5, 0)).to.equal('Error');
+  });
+
+  it('tests dividing zero, should return 0', function() {
+    expect(calculateNumber('DIVIDE', 0, -5)).to.equal(0);
+    expect(calculateNumber('DIVIDE', 0, 5)).to.equal(0);
+  });
+
+  it('tests invalid opertion type', function() {
+    expect(() => calculateNumber('MULT', 5, 6)).to.throw('Invalid operation');
+  });
+});
